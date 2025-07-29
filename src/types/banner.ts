4130ee5d5835431a037185a8f0cf6c742dd0e573
Wener Rodrigo <wener19.wr@gmail.com>
2025-07-28 22:08:08 -0300

    feat: created types application

---
+++ src/types/banner.ts
@@ -0,0 +1,11 @@
+export interface BannerConfig {
+    width: number;
+    height: number;
+    backgroundColor: string;
+    textColor: string;
+    text: string;
+    fontSize: number; 
+    fontFamily: string;
+    textX: number;
+    textY: number; 
+}
